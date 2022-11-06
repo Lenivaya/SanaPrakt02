bbccce3b7312277dd243573c1c1dc2377ae0ed7a
Lenivaya <xocada@gmail.com>
2022-11-06 15:34:40 +0200

    change last 2 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Курси");
 Console.WriteLine("Sana");
 Console.WriteLine("Commerce");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Software");
+Console.WriteLine("Development Bootcamp");
