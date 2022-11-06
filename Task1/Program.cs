70dc1fa7cbf9399e02a4ae96695ea6d1f1f66576
Lenivaya <xocada@gmail.com>
2022-11-06 15:34:09 +0200

    change first 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Курси");
+Console.WriteLine("Sana");
+Console.WriteLine("Commerce");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
